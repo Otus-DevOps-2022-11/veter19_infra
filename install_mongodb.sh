ba6fa5d6e98ef4c8942b84bb590b6ac230a60830
name <vet17@yandex.ru>
2023-01-06 18:51:34 +0300

    work

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+apt-get install apt-transport-https ca-certificates
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 apt-get update
